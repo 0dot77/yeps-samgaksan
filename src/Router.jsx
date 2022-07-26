b1854d3cc963c581ff851f7dac745cc1eeeb3421
0dot77 <yty0706@gmail.com>
2022-07-26 15:48:35 +0900

    feat: 임시 main 및 redirect 설정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RealtimeWishes from './pages/RealtimeWishes';
+import Main from './pages/Main';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/realtime-wishes" element={<RealtimeWishes />} />
       </Routes>
     </BrowserRouter>
